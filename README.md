8153ecb77b12202da1e507e49e97eddbdc73b738
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2020-03-17 12:00:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mini-Pascal compiler
+
+Compiler for Mini-Pascal language. This is an assignment for University of Helsinki course on Code Generation.
---
+++ src/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace mini_pascal {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
